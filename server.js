0379e8ea634196d3658865e4d76f35c011af81e6
brandondoran <bdoran@gmail.com>
2016-02-28 21:45:59 -0800

    Load babel require hook and the server from index

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-require('babel-register');
-const config = require('./config').default;
-const createApp = require('./app').default;
+import config from './config';
+import createApp from './app';
 
 const app = createApp(config);
 
-const server = app.listen(config.get('http').port, function() {
+const server = app.listen(config.get('http').port, () => {
   const port = server.address().port;
   console.log('==> listening on port %s', port); // eslint-disable-line no-console
 });
